11cee223990733ae79c5f14a542131a748cc5ff3
Аскар <michigan96@yandex.ru>
2019-04-21 19:10:00 +0500

    График прогноза погоды

--- src/index.js
+++ src/index.js
@@ -1,60 +1,102 @@
 import "babel-polyfill";
 import Chart from "chart.js";
 
-const currencyURL = "www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml";
-// const meteoURL = "/xml.meteoservice.ru/export/gismeteo/point/140.xml";
+const meteoURL = "/xml.meteoservice.ru/export/gismeteo/point/140.xml";
 
-async function loadCurrency() {
-  const response = await fetch(currencyURL);
+async function loadForecast(){
+  const response = await fetch(meteoURL);
   const xmlTest = await response.text();
   const parser = new DOMParser();
-  const currencyData = parser.parseFromString(xmlTest, "text/xml");
-  // <Cube currency="USD" rate="1.1321" />
-  const rates = currencyData.querySelectorAll("Cube[currency][rate]");
+  let forecastData = parser.parseFromString(xmlTest, "text/xml");
+  forecastData = forecastData.getElementsByTagName("FORECAST");
   const result = Object.create(null);
-  for (let i = 0; i < rates.length; i++) {
-    const rateTag = rates.item(i);
-    const rate = rateTag.getAttribute("rate");
-    const currency = rateTag.getAttribute("currency");
-    result[currency] = rate;
+
+  for (let i = 0; i < forecastData.length; i++) {
+    result[i] = new Object();
+    result[i].date = new Date(
+      forecastData[i].getAttribute("year"),
+      forecastData[i].getAttribute("month")-1,
+      forecastData[i].getAttribute("day"),
+      forecastData[i].getAttribute("hour")
+    );
+
+    result[i].dates = forecastData[i].getAttribute("day") + "." + forecastData[i].getAttribute("month") + "." + forecastData[i].getAttribute("year").slice(-2) + " " + forecastData[i].getAttribute("hour")+":00";
+
+    let temperatureMin = Number(forecastData[i].children[2].getAttribute("min"));
+    let temperatureMax = Number(forecastData[i].children[2].getAttribute("max"));
+    let heatMin = Number(forecastData[i].children[5].getAttribute("min"));
+    let heatMax = Number(forecastData[i].children[5].getAttribute("max"));
+
+    result[i].temperature = (temperatureMin + temperatureMax) / 2;
+    result[i].heat = (heatMin + heatMax) / 2;
   }
-  result["EUR"] = 1;
-  // result["RANDOM"] = 1 + Math.random();
+
   return result;
 }
 
-function normalizeDataByCurrency(data, currency) {
-  const result = Object.create(null);
-  const value = data[currency];
-  for (const key of Object.keys(data)) {
-    result[key] = value / data[key];
-  }
-  return result;
-}
+
 
 const buttonBuild = document.getElementById("btn");
 const canvasCtx = document.getElementById("out").getContext("2d");
+let forecastData;
 buttonBuild.addEventListener("click", async function() {
-  const currencyData = await loadCurrency();
-  const normalData = normalizeDataByCurrency(currencyData, "RUB");
-  const keys = Object.keys(normalData).sort((k1, k2) =>
-    compare(normalData[k1], normalData[k2])
+  forecastData = await loadForecast();
+  const keys = Object.keys(forecastData).sort((k1, k2) =>
+    compare(forecastData[k1].date, forecastData[k2].date)
   );
-  const plotData = keys.map(key => normalData[key]);
+  const plotTemperatureData = keys.map(key => forecastData[key].temperature);
+  const plotHeatData = keys.map(key => forecastData[key].heat);
+  const dates = keys.map(key => forecastData[key].dates);
 
+
+  function checkTemp()
+  {
+    if (forecastData[0].temperature<forecastData[0].heat)
+    return [
+      {
+        label: "Температура",
+        type: "line",
+        borderColor: "rgb(14, 108, 232)",
+        backgroundColor: "rgb(255, 255, 20)",
+        datasetFill:false,
+        data: plotTemperatureData
+      },
+      {
+        borderColor: "rgb(128, 5, 100)",
+        backgroundColor: "rgb(255, 20, 20)",
+        label: "Температура по ощущениям",
+        type: "line",
+        datasetFill:false,
+        data: plotHeatData
+      }
+    ]
+    else
+    return [
+      {
+        borderColor: "rgb(128, 5, 100)",
+        backgroundColor: "rgb(255, 20, 20)",
+        label: "Температура по ощущениям",
+        type: "line",
+        datasetFill:false,
+        data: plotHeatData
+      },
+      {
+        label: "Температура",
+        type: "line",
+        borderColor: "rgb(14, 108, 232)",
+        backgroundColor: "rgb(255, 255, 20)",
+        datasetFill:false,
+        data: plotTemperatureData
+      }
+    ]
+  };
+  
   const chartConfig = {
     type: "line",
 
     data: {
-      labels: keys,
-      datasets: [
-        {
-          label: "Стоимость валюты в рублях",
-          backgroundColor: "rgb(255, 20, 20)",
-          borderColor: "rgb(180, 0, 0)",
-          data: plotData
-        }
-      ]
+      labels: dates,
+      datasets: checkTemp()
     }
   };
 
@@ -65,13 +107,13 @@ buttonBuild.addEventListener("click", async function() {
       duration: 800,
       easing: "easeOutBounce"
     });
-  } else {
+  }else {
     window.chart = new Chart(canvasCtx, chartConfig);
   }
 });
 
 function compare(a, b) {
-  if (a > b) return 1;
-  if (a < b) return -1;
+  if (+a > +b) return 1;
+  if (+a < +b) return -1;
   return 0;
 }
